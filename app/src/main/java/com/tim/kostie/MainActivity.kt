531df7be9e6e15df79e827ff52cf47d5fd7fc13d
Fenyrs <fenyrahma9b@gmail.com>
2021-12-24 02:09:22 +0700

    ketinggalan ih

---
+++ app/src/main/java/com/tim/kostie/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tim.kostie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val collectionAdapter = CollectionAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager2)
+        viewPager.adapter = collectionAdapter
+    }
+}
