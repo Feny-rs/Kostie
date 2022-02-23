bff72a61338005ae24a8dccbceabaedff1f22068
fenyrahmasari <feny.rahmasari@ist.id>
2022-02-24 00:35:33 +0700

    onepage

--- app/src/main/java/com/tim/kostie/OnePage.kt
+++ app/src/main/java/com/tim/kostie/OnePage.kt
@@ -1,14 +1,22 @@
 package com.tim.kostie
 
+import android.annotation.SuppressLint
+import android.app.NotificationManager
 import android.content.Intent
 import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 
@@ -20,6 +28,7 @@ class OnePage : AppCompatActivity() {
 
         val database = Firebase.database
         val databaseReference = database.reference
+        var lamp = 0
 
         val temperatureProgressBar: ProgressBar = findViewById(R.id.temperatureProgressBar)
         val humidityProgressBar: ProgressBar = findViewById(R.id.humidityProgressBar)
@@ -27,22 +36,54 @@ class OnePage : AppCompatActivity() {
         val humidityTextView: TextView = findViewById(R.id.humidityTextView)
         val lampImage: ImageView = findViewById(R.id.lampu)
         val menuImageView: ImageView = findViewById(R.id.menuImageView)
-        menuImageView.setOnClickListener{
+        menuImageView.setOnClickListener {
             val intent = Intent(this, AboutApps::class.java)
             startActivity(intent)
         }
-        val lampRelay: MaterialButton = findViewById(R.id.button)
-        lampRelay.setOnClickListener{
-            if (!turnOn){
-                lampImage.setImageResource(R.drawable.lamp_on)
-                (lampImage.drawable as TransitionDrawable).startTransition(3000)
-                turnOn = true
-            } else {
-                lampImage.setImageResource(R.drawable.lampu_off)
-                (lampImage.drawable as TransitionDrawable).startTransition(3000)
-                turnOn=false;
+        val onButton: MaterialButton = findViewById(R.id.on)
+        val offButton: MaterialButton = findViewById(R.id.off)
 
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            @SuppressLint("SetTextI18n")
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val values = snapshot.child("Suhu").getValue<Float>()
+                val valuek = snapshot.child("Kelembapan").getValue<Float>()
+                lamp = snapshot.child("lampu").getValue<Int>()!!
+
+                temperatureProgressBar.progress = values?.toInt() ?: 0
+                temperatureTextView.text = "Suhu: "+values.toString()+" C°"
+
+                humidityProgressBar.progress = valuek?.toInt() ?: 0
+                humidityTextView.text = "Kelembapan: "+valuek.toString()+" %"
+
+                if (lamp == 0) {
+                    lamp = 1
+                } else {
+                    lamp = 0
+                }
+
+                if (values!! > 42){
+                    sendNotif()
+                }
             }
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+        })
+        onButton.setOnClickListener{
+            lampImage.setImageResource(R.drawable.lamp_on)
+            (lampImage.drawable as TransitionDrawable).startTransition(3000)
+            turnOn = false
+            databaseReference.child("lampu").setValue(lamp)
         }
+        offButton.setOnClickListener {
+            lampImage.setImageResource(R.drawable.lampu_off)
+            (lampImage.drawable as TransitionDrawable).startTransition(3000)
+            turnOn = true;
+            databaseReference.child("lampu").setValue(0)
+        }
+    }
+    fun sendNotif () {
+        val notif: NotificationManager
     }
 }
